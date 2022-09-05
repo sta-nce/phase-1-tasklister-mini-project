84ea37ff62496015a31df48ec53e2037405b0544
Stanslous <stancemuindi21@gmail.com>
2022-09-05 16:58:24 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let submitForm = document.querySelector('form')
+  submitForm.addEventListener ('submit',(e)=>{
+    e.preventDefault()
+    handleToDo(e.target.new-task-description.value)
+
+  }) 
 });
+ function handleToDo(todo){
+   let p = document.createElement('p') 
+   let btn = document.createElement('button')
+   btn.textContent ='x'
+   p.textContent = todo
+   p.appendChild(btn)
+   document.querySelector('list').appendChild(p)
+}
